cb034a2b0a6d15ebe13934f3c0ef54d64474aef4
projekt-gjh <34535341+projekt-gjh@users.noreply.github.com>
2018-01-11 21:11:24 +0100

    Update stranka_main.php

--- stranka_main.php
+++ stranka_main.php
@@ -1,4 +1,4 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
 <title>Súťaže na GJH</title>
@@ -8,31 +8,42 @@
 a{
 	color:inherit;
 }
-
 a:link {
     text-decoration: none;
 }
-
 a:visited {
     text-decoration: none;
 }	
-
 a:hover {
     text-decoration: underline;
 }
-
 .grid-container {
 	display: grid;
-	grid-template-columns: auto auto auto auto auto auto auto;
+	grid-template-columns: 150px auto auto auto auto auto auto auto auto auto;
+	background-color: #f6f4f8;
+}
+.header {
+	padding-left: 10px;
+	grid-area: 1 / 1 / 1 / 11;
+	font-size: 30px;
+	color: #ffffff;
+	background-color: #027202;
+}
+
+.menu {
+	grid-area: 2 / 1 / 40 /1;
+	background-color: #ffffff;
+	font-size: 20px;
+}
+
+.article1 {
+	grid-area: 3/5/8/10;
 	background-color: #ffffff;
 }
 
-.header {
-	grid-area: 1 / 1 / 1 / 8;
-	font-size: 30px;
-	color: #009900;
+.article2 {
+	grid-area: 10/5/15/10;
 }
-
 
 </style>
 </head>
@@ -40,20 +51,17 @@ a:hover {
 
 <div class="grid-container">
 	<div class="header"><a href='stranka_main.php'>Súťaže na GJH</a></div>
+
+<div class="menu">
+	<a href='stranka_main.php?type=tmf'>Turnaj Mladých Fyzikov</a>
+	<a href='stranka_main.php?type=robot'>Robotika</a>
+	<a href='stranka_main.php?type=debat'>Debatérsky klub</a>
+	<a href='stranka_main.php?type=sjl'>Slovenský jazyk a literatúra</a>
+	<a href='stranka_main.php?type=aj'>Anglický jazyk</a>
+	<a href='stranka_main.php?type=nj'>Nemecký jazyk</a>
+	<a href='stranka_main.php?type=mat'>Matematika</a>
+	<a href='stranka_main.php?type=fyz'>Fyzika</a>
 </div>
-
-<table>
-
-	<tr><td><a href='stranka_main.php?type=tmf'>Turnaj Mladých Fyzikov</a></td></tr>
-	<tr><td><a href='stranka_main.php?type=robot'>Robotika</a></td></tr>
-	<tr><td><a href='stranka_main.php?type=debat'>Debatérsky klub</a></td></tr>
-	<tr><td><a href='stranka_main.php?type=sjl'>Slovenský jazyk a literatúra</a></td></tr>
-	<tr><td><a href='stranka_main.php?type=aj'>Anglický jazyk</a></td></tr>
-	<tr><td><a href='stranka_main.php?type=nj'>Nemecký jazyk</a></td></tr>
-	<tr><td><a href='stranka_main.php?type=mat'>Matematika</a></td></tr>
-	<tr><td><a href='stranka_main.php?type=fyz'>Fyzika</a></td></tr>
-
-</table>	
 
 <?php
 	$mysqli=new mysqli( "localhost",
@@ -80,16 +88,31 @@ a:hover {
 	}
 	else{
 		$result = $mysqli->query("SELECT * FROM gjh_sutaze ORDER BY ID DESC LIMIT 2");
-		echo "Najnovšie oznámené súťaže<br>";
+		echo "Najnovšie oznámené súťaže <br>";
+		$i=0;
 		while($row=$result->fetch_assoc()){
-			echo "<br><a href='stranka_main.php?type=$row[event_type]&id=$row[ID]'>$row[competition_name]</a><br>";
-			echo "<br>$row[day].$row[month].$row[year]";
-			echo "<br>$row[minor_info]<br>";
+			$i++;
+			if($i==1){
+				echo "<div class='article1'>";
+				echo "<br><a href='stranka_main.php?type=$row[event_type]&id=$row[ID]'>$row[competition_name]</a><br>";
+				echo "<br>$row[day].$row[month].$row[year]";
+				echo "<br>$row[minor_info]<br>";
+				echo "</div>";
+			}
+			else{
+				echo "<div class='article2'>";
+				echo "<br><a href='stranka_main.php?type=$row[event_type]&id=$row[ID]'>$row[competition_name]</a><br>";
+				echo "<br>$row[day].$row[month].$row[year]";
+				echo "<br>$row[minor_info]<br>";
+				echo "</div>";
+			}
+			
 		}
 	}
 			
 	$mysqli->close();	
 ?>
 	
+</div>	
 </body>
 </html> 
